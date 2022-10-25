37cfab27f08ff48eae3fc8390f497c532c790bf4
👩Ladybug🐞 <psyjulga@yahoo.de>
2022-10-25 14:14:54 +0200

    added arr.fill method

--- arrayMethods.js
+++ arrayMethods.js
@@ -152,3 +152,23 @@ const arr3 = ['Robin']
 const children2 = arr1.concat(arr2, arr3)
 // console.log(children2)
 // [ 'Cecilie', 'Lone', 'Emil', 'Tobias', 'Linus', 'Robin' ]
+
+// ---- .FILL -----
+// The fill() method changes all elements in an array
+// to a static value, from a start index (default 0)
+// to an end index (default array.length).
+// It returns the modified array.
+// fill(value, start, end)
+
+const array1 = [1, 2, 3, 4]
+
+// fill with 0 from position 2 until position 4
+console.log(array1.fill(0, 2, 4))
+// expected output: [1, 2, 0, 0]
+
+// fill with 5 from position 1
+console.log(array1.fill(5, 1))
+// expected output: [1, 5, 5, 5]
+
+console.log(array1.fill(6))
+// expected output: [6, 6, 6, 6]
